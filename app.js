3722e9230bf01c21560d5df30f1dec37f6526a0b
EliTamošauskas <tamosauskas09@gmail.com>
2023-02-01 14:17:24 +0200

    changed text

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-	res.send("Hello server!");
+  res.send("Hello from your server!");
 });
 
 app.listen(port, () => console.log("Server running"));
